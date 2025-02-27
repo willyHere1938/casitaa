22be43c86a180ef7f82dc95a6d6d18ceb42125b9
willyHere1938 <157993624+willyHere1938@users.noreply.github.com>
2025-02-27 23:07:56 +0000

    fix: cambie carpeta sde lugar

---
+++ src/components/contenerdor.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="mi_primer_diseño">
+
+   </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .mi_primer_diseño{
+   background-color: blueviolet;
+   width: 300px;
+   height: 550px;
+   position: relative;
+  }
+
+</style>
